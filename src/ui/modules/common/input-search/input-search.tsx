1de8d728ed6432e4e8682489ea867297c59b7909
Erick Noiztbander <aventum2@gmail.com>
2024-11-19 23:29:55 +0100

    chore: add input search

--- src/ui/modules/common/input-search/input-search.tsx
+++ src/ui/modules/common/input-search/input-search.tsx
@@ -1,5 +1,36 @@
+"use client";
+
+import { ChangeEvent, useEffect, useState } from "react";
+import SearchIcon from "../icons/search-icon";
+import styles from "./input-search.module.css";
+import useDebounce from "@/ui/hooks/useDebounce";
+
 const InputSearch = () => {
-  return <>InputSearch</>;
+  const [query, setQuery] = useState("");
+  const debouncedInputValue = useDebounce(query, 500);
+
+  const fetchSearchResults = async (query: string) => {
+    console.log(query);
+  };
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  useEffect(() => {
+    fetchSearchResults(debouncedInputValue);
+  }, [debouncedInputValue]);
+
+  return (
+    <div className={styles.inputSearch}>
+      <input
+        value={query}
+        placeholder="Search the archive"
+        onChange={handleInputChange}
+      />
+      <SearchIcon />
+    </div>
+  );
 };
 
 export default InputSearch;
