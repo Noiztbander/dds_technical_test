ac2a6a93fc486ba77b3d748d14cef37168a3ab50
Erick Noiztbander <aventum2@gmail.com>
2024-11-19 12:59:33 +0100

    fix: change some setup in config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "http",
+        hostname: "cdna.artstation.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
